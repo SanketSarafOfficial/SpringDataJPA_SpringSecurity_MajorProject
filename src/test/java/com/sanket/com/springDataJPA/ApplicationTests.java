c0b58d7ee259b7c66d2f7650b9b2b9d4437bf83a
sanket Saraf <sanket.saraf.official@gmail.com>
2024-10-19 20:54:51 +0530

    Spring Data JPA changes

---
+++ src/test/java/com/sanket/com/springDataJPA/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.com.springDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
